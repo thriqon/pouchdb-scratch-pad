b1eac7194c4a0f518b4d9ddbe860ccf2b22a7433
Jonas Weber <github@jonasw.de>
2015-08-30 11:50:20 +0200

    Update creation.js

--- test/unit/creation.js
+++ test/unit/creation.js
@@ -11,8 +11,9 @@ describe('A scratch DB', function () {
   });
   it('shares it with another ScratchPad of the same name', function () {
     var db2 = new PouchDB({adapter: 'scratch', name: this.dbName});
+    var self = this;
     return db2.then(function () {
-      db2._db.should.be.equal(this.db._db);
+      db2._db.should.be.equal(self.db._db);
     });
   });
 });
