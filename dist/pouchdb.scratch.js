7d1e9fec7aa94cbb14f342f8032d446419147524
thriqon <github@jonasw.de>
2015-09-04 14:53:52 +0200

    adding dist files

--- dist/pouchdb.scratch.js
+++ dist/pouchdb.scratch.js
@@ -1,53 +1,99 @@
 (function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define('pouchdb-scratch', ['exports', 'module'], factory);
-  } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
-    factory(exports, module);
-  } else {
-    var mod = {
-      exports: {}
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  global.pouchdb-scratch = factory();
+}(this, function () { 'use strict';
+
+  var enqueue;
+
+  /* istanbul ignore else */
+  if (typeof process !== 'undefined') {
+    enqueue = function (cb, fst, scnd) {
+      process.nextTick(function () {
+        cb(fst, scnd);
+      });
     };
-    factory(mod.exports, mod);
-    global.pouchdbScratch = mod.exports;
-  }
-})(this, function (exports, module) {
-  'use strict';
-
-  var _ScratchPouch = 5;
-
-  function Database() {
-    return _ScratchPouch;
+  } else {
+    /* istanbul ignore next */
+    enqueue = function (cb, fst, scnd) {
+      setTimeout(function () {
+        cb(fst, scnd);
+      }, 0);
+    };
   }
 
-  //var utils = require('../pouch-utils');
+  function _getRevisionTree(id, callback) {
+    var errors = this.constructor.Errors;
+    callback(errors.error(errors.MISSING_DOC));
+  }
+
+  function info(callback) {
+    enqueue(callback, null, {
+      type: 'scratch pad',
+      name: this._dbName
+    });
+  }
+
+  /* global PouchDB */
+
+  var debug = PouchDB.debug('pouchdb:scratch');
+
+  function get(id, opts, callback) {
+    debug('_get', id, opts);
+
+    enqueue(callback, {status: 404}, null);
+  }
+
+  function _destroy (opts, callback) {
+    debug('_destroy', opts, callback);
+
+
+    /* istanbul ignore else *///pouchdb#4219
+    if (typeof opts === 'function') {
+      callback = opts;
+      opts = {};
+    }
+
+    this._db = null;
+
+    enqueue(callback, null, "ok");
+  }
+
   var databases = {};
 
   function getDatabase(name) {
     if (databases[name]) {
       return databases[name];
     } else {
-      return databases[name] = new Database();
+      return (databases[name] = {});
     }
   }
 
-  function ScratchPouch(opts) {
+  function ScratchPouch(opts, callback) {
     var api = this;
 
+    debug('instantiated');
     api._db = getDatabase(opts.name);
-    api.type = function () {
-      return 'scratch';
-    };
+    api._dbName = opts.name;
+    api.type = function () { return 'scratch'; };
+
+    api._destroy = _destroy;
+    api._get = get;
+    api._info = info;
+    api._getRevisionTree = _getRevisionTree;
+
+    enqueue(callback, null, "ok");
   }
 
+
   /* ScratchPouch is always possible */
-  ScratchPouch.valid = function () {
-    return true;
-  };
+  ScratchPouch.valid = function () { return true; };
 
   /* istanbul ignore next */
   if (typeof window !== 'undefined' && window.PouchDB) {
     window.PouchDB.adapter('scratch', ScratchPouch);
   }
 
-  module.exports = ScratchPouch;
-});
+  return ScratchPouch;
+
+}));
